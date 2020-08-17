148580cce864c0aa12c2349fc7abb80518ef17fa
vinothini-dhanasekar <vinothinidhanasekar0@gmail.com>
2020-08-17 15:54:01 +0530

    first

--- calculator.java
+++ calculator.java
@@ -1,2 +1,14 @@
-haiii java
-print something
+public class calculator{
+
+ 
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+ 
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
