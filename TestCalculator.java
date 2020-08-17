bb45c5d649a902b446f3a9c70e1e811a8a10184f
vinothini-dhanasekar <vinothinidhanasekar0@gmail.com>
2020-08-17 15:55:45 +0530

    second

--- TestCalculator.java
+++ TestCalculator.java
@@ -1,2 +1,14 @@
-hello java
-do something
+public class TestCalculator{
+
+ 
+
+   public static void main(String[] args) {
+        
+      int num1 = 20 num2 = 15, sum;
+      sum = num1 + num2;
+
+ 
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
